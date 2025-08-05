97269e5ae25a83de9b5c2bff6ac03c6dfcb880a2
Eduardo Nascimento <eduardo.san.nascimento@gmail.com>
2025-08-04 22:04:17 -0300

    5° Commit do Desenvolvimento do Sistema de Bosses
    Implementei umas linhas para assim que passar de level desbloquear o boss...
    
    Comecei a criar o chat da luta contra o boss...

--- Criando jogos/Jogo número 1 - Clicker lizard/JS/SystemMenuBoss/Fighters/fight.js
+++ Criando jogos/Jogo número 1 - Clicker lizard/JS/SystemMenuBoss/Fighters/fight.js
@@ -1,10 +1,19 @@
+import { attButtonBosses } from "../Bosses/bossUnlocked.js"
+
+// Função para colocar algum delay
 export function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 export async function fight(person, boss) {
     while (true) {
+
+        // if para ver quem começa de acordo com a velocidade do personagem contra o boss
         if (boss.getVelocidade >= person.getVelocidade) {
+
+            console.log(`--- ${boss.getName} VS Personagem ---`)
+
+            // if para ver se o boss tem mais dano que a defesa do personagem
             if (boss.getDano > person.getDefesa) {
                 person.setVida = person.getVida - (boss.getDano - person.getDefesa)
                 console.log(`O ${boss.getName} Atacou e deu ${(boss.getDano - person.getDefesa)} de Dano!`)
@@ -14,10 +23,12 @@ export async function fight(person, boss) {
             
             await delay(1000)
 
+            // Verificar se o Player morreu
             if (person.getVida <= 0) {
                 console.log(`Personagem Morreu! ${boss.getName} Venceu a Batalha!`)
                 return
             }
+            // if para ver se o personagem tem mais dano que a defesa do boss
             if (person.getDano > boss.getDefesa) {
                 boss.setVida = boss.getVida - (person.getDano - boss.getDefesa)
                 console.log(`O Personagem atacou e deu ${(person.getDano - boss.getDefesa)} de Dano!`)
@@ -27,12 +38,18 @@ export async function fight(person, boss) {
             
             await delay(1000)
 
+            //  Verificar se o Boss morreu
             if (boss.getVida <= 0) {
                 console.log(`${boss.getName} Morreu! Personagem Venceu a Batalha!`)
                 boss.setVivo = "Derrotado"
+                attButtonBosses()
                 return
             }
         } else {
+
+            console.log(`--- Personagem VS ${boss.getName} ---`)
+
+            // if para ver se o personagem tem mais dano que a defesa do boss
             if (person.getDano > boss.getDefesa) {
                 boss.setVida = boss.getVida - (person.getDano - boss.getDefesa)
                 console.log(`O Personagem atacou e deu ${(person.getDano - boss.getDefesa)} de Dano!`)
@@ -42,11 +59,14 @@ export async function fight(person, boss) {
             
             await delay(1000)
 
+            //  Verificar se o Boss morreu
             if (boss.getVida <= 0) {
                 console.log(`${boss.getName} Morreu! Personagem Venceu a Batalha!`)
                 boss.setVivo = "Derrotado"
+                attButtonBosses()
                 return
             }
+            // if para ver se o boss tem mais dano que a defesa do personagem
             if (boss.getDano > person.getDefesa) {
                 person.setVida = person.getVida - (boss.getDano - person.getDefesa)
                 console.log(`O ${boss.getName} Atacou e deu ${(boss.getDano - person.getDefesa)} de Dano!`)
@@ -56,6 +76,7 @@ export async function fight(person, boss) {
             
             await delay(1000)
 
+            // Verificar se o Player morreu
             if (person.getVida <= 0) {
                 console.log(`Personagem Morreu! ${boss.getName} Venceu a Batalha!`)
                 return
--- Criando jogos/Jogo número 1 - Clicker lizard/JS/SystemMenuBoss/ScriptsBosses/scriptsBosses.js
+++ Criando jogos/Jogo número 1 - Clicker lizard/JS/SystemMenuBoss/ScriptsBosses/scriptsBosses.js
@@ -2,7 +2,7 @@ import { MyPerson } from "../Personagem/myPerson.js";
 import { CreateBoss } from "../Bosses/CreateBoss.js";
 import { fight } from "../Fighters/fight.js";
 
-export let myPersonGame = new MyPerson(1, 5, 1, 20, 1, 101, 0.1)
+export let myPersonGame = new MyPerson(1, 50000, 1, 20, 100, 101, 0.1)
 
 export let bossGoblin = new CreateBoss(1, "Boss Goblin", 2, "Vivo", 20, 5, 100, 10, 100, 0.01)
 export let bossLobo = new CreateBoss(1, "Boss Lobo", 5, "Vivo", 80, 30, 500, 30, 150, 1)
